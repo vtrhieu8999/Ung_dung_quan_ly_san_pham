b0e3e9dd37e3771d8089b82f0b343742f8db6858
Vuong_Trung_Hieu <v.hieu1998@gmail.com>
2020-08-25 10:07:58 +0700

    quan ly san pham

---
+++ script.js
@@ -0,0 +1,32 @@
+let proArr= new Array();
+
+function addProToArr(){
+    proArr.push(document.getElementById('input').value);
+    displayPro();
+}
+
+function displayPro(){
+    let doc=`<tr>
+                 <th colspan="3">Product name</th>
+                 <th id="Amount">Amount:  ` +proArr.length+ `</th>
+            </tr>`;
+    for(i=0; i< proArr.length; i++){
+        doc+= `<tr>`;
+            doc+= `<td>`;
+                doc+= proArr[i];
+            doc+= `</td>`;
+            doc+= `<td><button onclick="edit(` +i+  `)">Edit</button></td>`;
+            doc+= `<td><button onclick="del(` +i+  `)">Delete</button></td>`;
+        doc+= `</tr>`;
+    }
+    document.getElementById('myTable').innerHTML= doc;
+}
+
+function del(index){
+    proArr.splice(index, 1);
+    displayPro();
+}
+function edit(index){
+    proArr[index]= prompt('Doi ten san pham', proArr[index]);
+    displayPro();
+}
